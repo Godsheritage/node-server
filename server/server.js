26040fd2adbefcdc5d0e963a93fb2c1c7637bd9b
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-09 10:40:00 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const http = require('http')
+const data = require('./data.json')
 
 server = http.createServer((req, res) => {
-    res.writeHead(200, )
-    res.end({})
-})
+    res.writeHead(200, {
+        "Content-Type" : "application/json" 
+    })
+    res.end(data)
+}).listen(3000)
+
