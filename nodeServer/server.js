bfd081dda5c0cd58ed2eefe60453d30da92dcfde
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-09 10:34:22 +0100

    first ever node server

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,9 +1,13 @@
-const http = require('http')
+const http = require("http");
 
 //create server object
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     // write response
-    res.write('Hello world')
-    res.end()
-}).listen(5000, () => {console.log('Server runing...')})
+    res.write("Hello world");
+    res.end();
+  })
+  .listen(5000, () => {
+    console.log("Server runing...");
+  });
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+
+server = http.createServer((req, res) => {
+    res.writeHead(200, )
+    res.end({})
+})
