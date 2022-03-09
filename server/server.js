91549bdabe3ebedb468401fc6ec7b213d5729761
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-09 10:43:42 +0100

    very first node server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-const http = require('http')
-const data = require('./data.json')
+const http = require("http");
+const data = require("./data.json");
 
-server = http.createServer((req, res) => {
+server = http
+  .createServer((req, res) => {
     res.writeHead(200, {
-        "Content-Type" : "application/json" 
-    })
-    res.end(data)
-}).listen(3000)
-
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(data));
+  })
+  .listen(3000, () => {
+    console.log("Your server is running");
+  });
