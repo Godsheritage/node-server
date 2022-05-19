7dedfaa399fb5635d87356fd4426227603198505
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-19 21:39:30 +0100

    project restructuring

--- adeoye-dynasty/src/Home/Home.tsx
+++ adeoye-dynasty/src/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Main from "./Main";
-import Footer from "../../shared/Footer";
+import Footer from "../shared/Footer";
 import MainNavigation from "../../components/navigation/MainNavigation";
 
 const Home = () => {
